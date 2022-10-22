ad2fbdede6778f262f793d8a215d6e0be911047e
NaityDhim <florian.abry@wanadoo.fr>
2022-10-22 21:54:32 +0200

    second commit

---
+++ public/js/my-scripts.js
@@ -0,0 +1,23 @@
+function calculate_age(birth_day,birth_month,birth_year)
+{
+    today_date = new Date();
+    today_year = today_date.getFullYear();
+    today_month = today_date.getMonth();
+    today_day = today_date.getDate();
+    age = today_year - birth_year;
+
+    if ( today_month < (birth_month - 1))
+    {
+        age--;
+    }
+    if (((birth_month - 1) == today_month) && (today_day < birth_day))
+    {
+        age--;
+    }
+    document.write(age);
+}
+
+function test()
+{
+    document.write("You are learning how to call JavaScript function in html");
+}
